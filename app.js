4e85efc06c12ca9564e5c0a2294e4d491517d5cc
Ayesha Simran <ayeshasimran@Ayeshas-MacBook-Pro.local>
2025-12-28 16:24:35 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,121 @@
+const express = require('express');
+const app = express();
+const userModel = require('./models/user');
+const postModel = require('./models/post');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const Path = require('path');
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(Path.join(__dirname, 'public')));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.get('/profile', isloggedIn, async (req, res) => {
+  let user = await userModel.findOne({ email: req.user.email }).populate('posts');
+  res.render('profile', { user });
+});
+
+app.get('/like/:id', isloggedIn, async (req, res) => {
+  let post = await postModel.findOne({ _id: req.params.id });
+
+  if (post.likes.indexOf(req.user.userid) === -1) {
+    post.likes.push(req.user.userid);
+  } else {
+    post.likes.splice(post.likes.indexOf(req.user.userid), 1);
+  }
+
+  await post.save();
+  res.redirect('/profile');
+});
+
+app.get('/edit/:id', isloggedIn, async (req, res) => {
+  let post = await postModel.findOne({ _id: req.params.id });
+  res.render('edit', { post });
+});
+app.get('/delete/:id', isloggedIn, async (req, res) => {
+  let post = await postModel.findOneAndDelete({ _id: req.params.id });
+  res.redirect('/profile');
+});
+app.post('/update/:id', isloggedIn, async (req, res) => {
+  let post = await postModel.findOneAndUpdate({ _id: req.params.id}, { content: req.body.content });
+  res.redirect('/profile');
+});
+app.post('/post', isloggedIn, async (req, res) => {
+  let user = await userModel.findOne({ email: req.user.email });
+  let { content } = req.body;
+
+  let post = await postModel.create({
+    user: user._id,
+    content
+  });
+
+  user.posts.push(post._id);
+  await user.save();
+  res.redirect('/profile');
+});
+
+app.post('/register', async (req, res) => {
+  let { username, email, name, password, age } = req.body;
+  let user = await userModel.findOne({email});
+  if (user) return res.status(500).send('User already exists');
+
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      let user = await userModel.create({
+        username,
+        name,
+        email,
+        password: hash,
+        age,
+      });
+      let token = jwt.sign({ email: email, userid: user._id },'secretkey'
+      );
+      res.cookie('token', token);
+      res.redirect('/login');
+    });
+  });
+});
+
+app.post('/login', async (req, res) => {
+  let { email, password } = req.body;
+  let user = await userModel.findOne({ email });
+  if (!user) return res.status(500).send('Something went wrong');
+
+  bcrypt.compare(password, user.password, (err, result) => {
+    if (result) {
+      let token = jwt.sign({ email: email, userid: user._id },'secretkey'
+      );
+      res.cookie('token', token);
+      res.redirect('/profile');
+    } else {
+      res.redirect('/login');
+    }
+  });
+});
+
+app.get('/logout', (req, res) => {
+  res.clearCookie('token');
+  res.redirect('/login');
+});
+
+function isloggedIn(req, res, next) {
+  if (!req.cookies.token) return res.redirect('/login');
+  else{
+  let data = jwt.verify(req.cookies.token, 'secretkey');
+  req.user = data;
+  next();
+  }
+}
+
+app.listen(3000);
